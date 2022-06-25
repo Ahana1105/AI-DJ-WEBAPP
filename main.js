dbd2e51ceeb76bfc2eadd10f6058851b448a3c1b
Ahana1105 <77888005+Ahana1105@users.noreply.github.com>
2022-06-25 10:25:59 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
-song ="";
+song = "";
+leftWristX = 0;
+rightWristX = 0;
+leftWristY = 0;
+rightWristY = 0;
+
 
 function preload() {
-   song= loadSound("music.mp3")
+    song = loadSound("music.mp3")
 }
 
 
@@ -12,7 +17,31 @@ function setup() {
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("Left Wrist X =" + leftWristX + "Left Wrist Y = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("Right Wrist X =" + rightWristX + "Right Wrist Y = " + rightWristY);
+    }
+
+}
+
+function modelLoaded() {
+    console.log("Posenet model is initialised");
+}
+
+
 
 function draw() {
     image(video, 0, 0, 600, 500);
@@ -20,5 +49,7 @@ function draw() {
 
 function play() {
     song.play();
+    song.setVolume(1);
+    song.rate(1);
 }
 
